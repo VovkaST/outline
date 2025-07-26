30c7df6432d3de02ce49cd5e2ddcee4da166ea91
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-07-27 00:30:08 +0300

    #28: Базовая инициализация эндпоинтов работы с ключами

--- app_server/routes/keys.py
+++ app_server/routes/keys.py
@@ -1,5 +1,17 @@
 from fastapi import APIRouter
+from starlette.requests import Request
 
+from app_server import dtos
 from root.utils.others import get_route_name
 
 routes = APIRouter(tags=["Keys"], prefix="/api/keys", generate_unique_id_function=get_route_name)
+
+
+@routes.put("/")
+async def create_or_update_key(request: Request, payload: dtos.PutKeyRequest):
+    """Создать или изменить ключ доступа."""
+
+
+@routes.get("/{key_id}")
+async def get_key_file(request: Request, key_id: str):
+    """Получить файл ключа доступа."""
