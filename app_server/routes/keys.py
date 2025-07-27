d7ba57051d090e724e1d3049ebc653793efe9c8f
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-07-27 15:56:11 +0300

    #28: Добавил эндпоинт скачивания файла ключа

--- app_server/routes/keys.py
+++ app_server/routes/keys.py
@@ -1,7 +1,11 @@
+import pathlib
+
 from fastapi import APIRouter
+from fastapi.responses import FileResponse
 from starlette.requests import Request
 
 from app_server import dtos
+from keys.storage import keys_storage
 from root.utils.others import get_route_name
 
 routes = APIRouter(tags=["Keys"], prefix="/api/keys", generate_unique_id_function=get_route_name)
@@ -10,12 +14,14 @@ routes = APIRouter(tags=["Keys"], prefix="/api/keys", generate_unique_id_functio
 @routes.put("/")
 async def create_or_update_key(request: Request, payload: dtos.PutKeyRequest):
     """Создать или изменить ключ доступа."""
-    from keys.storage import keys_storage
 
     file_name = keys_storage.make_name(payload.guid)
-    keys_storage.write(file_name, key=payload.key)
+    keys_storage.save(file_name, key=payload.key)
 
 
 @routes.get("/{key_id}")
 async def get_key_file(request: Request, key_id: str):
     """Получить файл ключа доступа."""
+    path = keys_storage.get(key_id)
+    file_name = pathlib.Path(path).name
+    return FileResponse(path, filename=file_name)
