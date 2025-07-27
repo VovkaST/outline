93d4958da9108f7dccc0c2e815b3fed85a845feb
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-07-27 15:55:44 +0300

    #28: Добавил методы работы с файловым хранилищем

--- keys/storage.py
+++ keys/storage.py
@@ -6,10 +6,8 @@ class KeysStorage(LocalStorage):
     def make_name(self, guid: str) -> str:
         return f"{guid}.txt"
 
-    def write(self, file_name: str, key: str):
-        file_path = self.build_absolute(file_name)
-        with open(file_path, mode="w", encoding="utf8") as file:
-            file.write(key)
+    def get(self, name: str) -> str:
+        return super().get(name=self.make_name(name))
 
 
 keys_storage = KeysStorage(settings.MEDIA_DIR / "keys")
