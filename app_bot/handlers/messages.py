c897c800b00913aa16e7291a03ea9123477c2e39
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-09-05 20:33:37 +0300

    #35 Доработал прием медиа файлов

--- app_bot/handlers/messages.py
+++ app_bot/handlers/messages.py
@@ -16,19 +16,26 @@ async def user_message_handler(update: Update, context: ContextTypes.DEFAULT_TYP
     )
 
 
-async def user_message_with_photo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def user_message_with_attachment_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user: User = update.message.from_user  # type: ignore [attr-not-none]
     telegram_id = str(user.id)
     username = clear_username(user.username)
 
-    photo_file = await update.message.photo[-1].get_file()
-    attachment_name = photo_file.file_path.split("/")[-1]
+    async def get_file_path(file) -> str:
+        f = await file.get_file()
+        return f.file_path
+
+    file_path = ""
+    if update.message.photo:
+        file_path = await get_file_path(update.message.photo[-1])
+    elif update.message.effective_attachment:
+        file_path = await get_file_path(update.message.effective_attachment)
 
     await planfix_webchat.chat.new_message(
         chat_id=telegram_id,
         contact_id=telegram_id,
         contact_name=username,
         message="",
-        attachment_name=attachment_name,
-        attachment_url=photo_file.file_path,
+        attachment_name=file_path.split("/")[-1] if file_path else "",
+        attachment_url=file_path,
     )
