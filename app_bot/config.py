bb8099fc3d398f0a3d618c134791a237fbf5f60a
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-09-22 00:15:33 +0300

    #40. Изменил ссылку на приложение IOS

--- app_bot/config.py
+++ app_bot/config.py
@@ -5,7 +5,7 @@ class BotAppConfig(BaseAppConfig):
     PREFIX = "BOT"
 
     TOKEN: str = ""
-    APP_URL_IOS: str = "https://apps.apple.com/us/app/happ-proxy-utility/id6504287215"
+    APP_URL_IOS: str = "https://apps.apple.com/ru/app/happ-proxy-utility-plus/id6746188973"
     APP_URL_ANDROID: str = "https://play.google.com/store/apps/details?id=com.happproxy"
 
     HOOK_REQUEST_DATA_PREFIX: str = "HOOK_"
