98e72e994ff15fb500a4e99b4d031e25c17f4a31
Korotkov Vladimir <vovka.steel911@mail.ru>
2026-06-02 22:16:36 +0300

    feat: [#108] Отображается номер телефона или номер задачи в зависимости от источника.

--- app_server/assets/src/components/tariffs/SubscriptionRef.component.vue
+++ app_server/assets/src/components/tariffs/SubscriptionRef.component.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-defineProps<{
+import { computed } from 'vue';
+
+const props = defineProps<{
   subscriptionNumber: string;
 }>();
+
+const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+const isUuid = computed(() => UUID_REGEX.test(props.subscriptionNumber));
 </script>
 
 <template>
-  <div class="sub-ref">
+  <div class="sub-ref" :class="{ 'sub-ref--stacked': isUuid }">
     <div class="sub-ref-label">Оплата подписки</div>
-    <div class="sub-ref-number">№{{ subscriptionNumber }}</div>
+    <div class="sub-ref-number">№ {{ subscriptionNumber }}</div>
   </div>
 </template>
 
@@ -30,11 +36,21 @@ defineProps<{
   font-weight: 500;
 }
 
+.sub-ref--stacked {
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+
 .sub-ref-number {
   font-size: 15px;
   font-weight: 700;
   color: var(--primary);
   font-feature-settings: 'tnum';
   letter-spacing: 0.3px;
+}
+
+.sub-ref--stacked .sub-ref-number {
+  word-break: break-all;
 }
 </style>
